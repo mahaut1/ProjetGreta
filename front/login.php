5d5677bf91b5979eaa451bb763241fc61c844fac
mahaut1 <mwindal@hotmail.com>
2023-05-12 12:18:12 +0200

    suppression config.php ajout photos
    suppression config.php ajout photos

--- front/login.php
+++ front/login.php
@@ -1,80 +1,12 @@
-<?php
+<div class="login" action="">
+		<form method="POST">
 
-require_once 'functions.php';
-
-// Création d'un bloc try/catch pour gérer les exceptions de la BDD
-try {
-    // Connection à la BDD
-    $db = connect();
-
-    // Création de $membersQuery. On utilise inner join pour récupérer les titres d'abo
-    $membersQuery = $db->query('SELECT members.*, abos.titre FROM members INNER JOIN abos ON members.abo_id = abos.id');
-    // Récupération de tous les membres et assignation à $members
-    $members = $membersQuery->fetchAll(PDO::FETCH_ASSOC);; // tableaux associatifs
-
-} catch (Exception $e) {
-    // Affiche le message en cas d'exception
-    echo $e->getMessage();
-}
-
-// Fermeture de la connexion à la BDD
-$db=null;
-?>
-
-<?php require_once 'header.php' ?>
-
-<a href='index.php' class='btn btn-secondary m-2 active' role='button'>Accueil</a>
-<a href='abos.php' class='btn btn-secondary m-2 active' role='button'>Abos</a>
-
-<?php if (!empty($_GET['type']) && ($_GET['type'] === 'success')) : ?>
-    <div class='row'>
-        <div class='alert alert-success'>
-            Succès! <?= $_GET['message'] ?>
-        </div>
-    </div>
-<?php elseif (!empty($_GET['type']) && ($_GET['type'] === 'error')) : ?>
-    <div class='row'>
-        <div class='alert alert-danger'>
-            Erreur! <?= $_GET['message'] ?>
-        </div>
-    </div>
-<?php endif; ?>
-<div class='row'>
-    <h1 class='col-md-12 text-center border border-dark text-white bg-primary'>Membres</h1>
+			<label for="chk" aria-hidden="true">Login</label>
+			<input type="hidden" name="action" value="login">
+			<input type="email" name="email" placeholder="Email" required="">
+			<input type="password" name="pwd" placeholder="Mot de passe" required="">
+			<button>Login</button>
+			<a href="?p=forgot">Mot de passe oublié ?</a><!-- Je change le paramètre d'URL à forgot, mais je reste sur l'index
+		</form>
+	</div>
 </div>
-<div class='row'>
-    <table class='table table-striped'>
-        <thead>
-            <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Prénom</th>
-                <th scope='col'>Nom</th>
-                <th scope='col'>Adresse</th>
-                <th scope='col'>Abo</th>
-                <th scope='col'>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($members as $member) : ?>
-                <tr>
-                    <td><?= $member['id'] ?></td>
-                    <td><?= htmlentities($member['prenom']) ?></td>
-                    <td><?= htmlentities($member['nom']) ?></td>
-                    <td><?= htmlentities($member['adresse']) ?></td>
-                    <td><?= htmlentities($member['titre']) ?></td>
-                    <td>
-                        <a class='btn btn-primary' href='member-form.php?id=<?= $member['id'] ?>' role='button'>Modifier</a>
-                        <a class='btn btn-danger' href='delete-member.php?id=<?= $member['id'] ?>' role='button'>Supprimer</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
-<div class='row'>
-    <div class='col'>
-        <a class='btn btn-success' href='member-form.php' role='button'>Ajouter membre</a>
-    </div>
-</div>
-
-<?php require_once 'footer.php' ?>
--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +7,35 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>Site de petites annonces</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Site de petites annonces communautaire</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Membres</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Annonces</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Catégories</a>
+          </li>
+        
+        </ul>
+        <span class="navbar-text">
+          Site de petites annonces communautaire
+        </span>
+      </div>
+    </div>
+  </nav>

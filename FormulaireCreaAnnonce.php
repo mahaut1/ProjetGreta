668407eb64bf0725887e35fd1b4d5b998f4797af
mahaut1 <mwindal@hotmail.com>
2023-05-13 17:22:23 +0200

    formulaire sign up et Crea annonce

--- FormulaireCreaAnnonce.php
+++ FormulaireCreaAnnonce.php
@@ -18,7 +18,7 @@
 	</div>
 </div>
 
-
+<form method="">
  <div class="mb-3">
   <label for="Titre_Annonce" class="form-label">Titre de l'annonce</label>
   <input type="text" class="form-control" name ="Titre_annonce" id="Titre_annonce" placeholder="Votre titre">
--- front/signup.php
+++ front/signup.php
@@ -1,27 +1,90 @@
-
-
-
-<div class="main">  	
-	<input type="checkbox" id="chk" aria-hidden="true">
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Site de petites annonces</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Site de petites annonces</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Site de petites annonces communautaire</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="http://localhost/ProjetGreta/index.html">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Membres</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Annonces</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Catégories</a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link" href="#">se connecter</a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link" href="/front/signup.php">S'inscrire</a>
+          </li>
+        
+        </ul>
+        <span class="navbar-text">
+          Site de petites annonces communautaire
+        </span>
+      </div>
+    </div>
+  </nav>
+<div class="main">
+<input type="chekbox" id="signup" aria-hidden="true">	
 	<div class="signup">
-		<form method="POST" action="">
-			<label for="chk" aria-hidden="true">Enregistrement</label>
-			<input type="hidden" name="action" value="signup"> 
+	<form method="POST" action="/data/users.csv">
+	<div class="mb-3">
+			<label for="Username" class="form-label">username</label>
+			<input type="text" name="username" placeholder="username" required="">
+			</div>
+			<div class="mb-3">
+			<label for="email" class="form-label">Votre email</label>
+			<input type="text" name="nom" placeholder="email" required="">
+			</div>
+			<div class="mb-3">
+			<label for="Nom" class="form-label">Votre Nom</label>
 			<input type="text" name="nom" placeholder="Nom" required="">
-            <input type="text" name="prenom" placeholder="prenom">
-            <input type="date" name="date_naissance" placeholder="date_naissance">
-            <input type ="tel" name="telephone" placeholder="telephone">
-            <input type="text" name="username" placeholder="username">
-			<input type="email" name="email" placeholder="Email" required=""> 
-            <input type="text" name="adresse_postale" placeholder="adresse_postale">
-            <input type="text" name="code_postal" placeholder="code_postal">
-            <input type="text" name="ville" placeholder="ville">
-            <input type="datetime-local" name="date_inscription" placeholder="date_inscription">
-			<input type="password" name="pwd" placeholder="Mot de passe" required="" pattern="^(?=.*\d)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,}$" title="Le mot de passe doit comporter au moins 8 caractères dont au moins 1 chiffre, 1 minuscule, 1 majuscule et 1 caractères spécial">
-			<input type="password" name="pwd2" placeholder="Confirmation du mot de passe" required="">
-			<button>Enregistrement</button>
-		</form>
-	</div>
-
-
-
+			</div>
+			<div class="mb-3">
+			<label for="Prénom" class="form-label">Votre Prénom</label>
+			<input type="text" name="prénom" placeholder="prénom" required="">
+			</div>
+			<div class="mb-3">
+			<label for="Téléphone" class="form-label">Votre numero de téléphone</label>
+			<input type="tel" name="telephone" placeholder="telephone">
+			</div>
+			<div class="mb-3">
+			<label for="Date_naissance" class="form-label">Votre date de naissance</label>
+			<input type="date" name="date_naissance" placeholder="Votre date de naissance">
+			</div>
+			<div class="mb-3">
+			<label for="Adresse_postale" class="form-label">Votre adresse postale</label>
+			<input type="text" name="adresse_postale" placeholder="Votre adresse potale">
+			</div>
+			<div class="mb-3">
+			<label for="Code_postal" class="form-label">Votre code postal</label>
+			<input type="text" name="code_postal" placeholder="Votre code postale">
+			</div>
+			<div class="mb-3">
+			<label for="Date_naissance" class="form-label">Votre date de naissance</label>
+			<input type="date" name="date_naissance" placeholder="Votre date de naissance">
+			</div>
+			<div class="mb-3">
+		
+</form>

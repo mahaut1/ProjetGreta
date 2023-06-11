a76207ab1384e99652454cea3cc211662f2235fc
mahaut1 <mwindal@hotmail.com>
2023-06-11 17:15:27 +0200

    modification de l'index et de l'header-admin

--- index.php
+++ index.php
@@ -1,66 +1,122 @@
 <?php
 session_start();
 require_once "modele.php";
+require_once "controllers.php";
 
 
-$p= $_GET['p'] ?? "";
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if($_SERVER["REQUEST_METHOD"] === "POST"){
-	$action= $_POST['action'] ?? "";
-	switch ($action){
-		case 'signup':
-			$message=addUser();
-			break;
-		case 'login':
-			$message=logUser();
-			$p="home";
-			break;
-		case 'forgot':
-			$message=waitReset();
-			$p="home";
-			break;
-		case 'reset':
-			$message=resetPwd();
-			$p="signup";
-	}
+if (strpos($uri, "admin") !== false) {
+    include_once "_partials/header_admin.php";
+} else {
+    $header_categories = getCategories();
+    include_once "_partials/header.php";
 }
 
-if ($p=='activation')
-		$message=activUser();
-if ($p=='deconnect'){
-	session_unset();
-	session_destroy();
-	$message=array("success", "Vous êtes déconnecté");
+?>
+
+<?php
+if ('/index.php' == $uri)
+{
+    echo index();
 }
-if ($p=='reset' && !isset($_GET['t'])){
-	$message=array("error", "Lien invalide. Veuillez réessayer");
-	$p="forgot";
+elseif ('/index.php/products' == $uri && isset($_GET['id']))
+{
+    echo products($_GET['id']);
+}
+elseif ('/index.php/product' == $uri && isset($_GET['id']))
+{
+    echo product($_GET['id']);
+}
+elseif ('/index.php/login' == $uri)
+{
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        echo logUser();
+    } else {
+        echo login();
+    }
+}
+elseif ('/index.php/logout' == $uri)
+{
+    $_SESSION["logged"] = false;
+    $_SESSION["id"] = null;
+    $_SESSION["email"] = null;  
+    header('Location: /index.php');
+    exit();   
 }
 
-$logged = $_SESSION['is_login'] ?? false;
-
-include "view/header.php";
-switch ($p) {
-	case 'forgot':
-		include "view/forgot.php";	
-		break;	
-	case 'reset':
-		$token=htmlspecialchars($_GET['t']);
-		include "view/reset.php";	
-		break;	
-	case 'signup':
-		include "front/signup.php";	
-		break;
-	case 'se connecter':
-		include "front/login.php";
-		break;
-	case 'ajouter une annonce':
-		include "front/FormulaireCreaAnnonce.php";
-		break;
-	case 'contact':
-		include "front/contact.php";
-		break;
-	default:
-		include "view/home.php";	
+elseif ('/index.php/signup' == $uri)
+{
+    echo register();
 }
-include "view/footer.php";
+elseif ('/index.php/admin/index' == $uri)
+{
+    echo admin_index();
+}
+elseif ('/index.php/admin/admins' == $uri)
+{
+    echo admin_admins();
+}
+elseif ('/index.php/admin/categories' == $uri)
+{
+    echo admin_categories();
+}
+elseif ('/index.php/admin/category/add' == $uri)
+{
+    echo admin_category_add();
+}
+elseif ('/index.php/admin/products' == $uri)
+{
+    echo admin_products();
+}
+elseif ('/index.php/admin/products/add' == $uri)
+{
+    echo admin_product_add();
+}
+elseif ('/index.php/admin/category/del' == $uri)
+{
+    echo admin_category_del($_GET['id']);
+}
+elseif ('/index.php/admin/categories/import' == $uri)
+{
+    echo admin_categories_import();
+}
+elseif ('/index.php/admin/product/del' == $uri)
+{
+    echo admin_remove_product($_GET['id']);
+}
+elseif ('/index.php/admin/admin/del' == $uri)
+{
+    echo admin_remove_user($_GET['id']);
+}
+elseif ('/index.php/admin/user/del' == $uri)
+{
+    echo admin_remove_user($_GET['id']);
+}
+elseif ('/index.php/admin/user/add' == $uri)
+{
+    echo addUser();
+}
+elseif ('/index.php/admin/user/import' == $uri)
+{
+    echo admin_user_import();
+}
+elseif ('/index.php/admin/users' == $uri)
+{
+    echo admin_users();
+}
+elseif ('/index.php/panier/add' == $uri && isset($_GET['id']))
+{
+    echo add_panier($_GET['id']);
+}
+elseif ('/index.php/panier/del' == $uri && isset($_GET['id']))
+{
+    echo del_panier($_GET['id']);
+}
+else
+{
+    echo index();
+}
+?>
+
+<?php include_once "_partials/footer.php"; ?>
